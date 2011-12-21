b3ccc7c0f7dc20ce3785c7b122bb870cc3b66f73
eric.bodden <eric.bodden@localhost>
2011-12-21 10:14:31 +0100

    .

--- MOPBox/src/de/bodden/mopbox/finitestate/sync/NumberSyncFSMMonitorTemplate.java
+++ MOPBox/src/de/bodden/mopbox/finitestate/sync/NumberSyncFSMMonitorTemplate.java
@@ -3,11 +3,14 @@ package de.bodden.mopbox.finitestate.sync;
 import helpers.FailSafeIterMonitorTemplate;
 import helpers.FailSafeIterMonitorTemplate.Var;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 import de.bodden.mopbox.finitestate.OpenFSMMonitorTemplate;
@@ -16,6 +19,7 @@ import de.bodden.mopbox.generic.IAlphabet;
 import de.bodden.mopbox.generic.IIndexingStrategy;
 import de.bodden.mopbox.generic.ISymbol;
 import de.bodden.mopbox.generic.IVariableBinding;
+import de.bodden.mopbox.generic.def.VariableBinding;
 
 /**
  * Idea: can use this technique to buffer event and then process them in bursts.
@@ -83,7 +87,10 @@ public abstract class NumberSyncFSMMonitorTemplate<L, K, V> extends OpenFSMMonit
 				}
 				
 				delta++;
-			} while(!frontier.equals(nextFrontier));
+			//in the following conditional, the 2nd branch is to make sure that we create
+			//transitions for all symbols in the alphabet; alternatively we could do a
+			//dynamic transition matching
+			} while(!frontier.equals(nextFrontier) || delta<maxNumber());
 		}
 				
 		return stateFor(Collections.singleton(delegate.getInitialState()));
@@ -129,6 +136,11 @@ public abstract class NumberSyncFSMMonitorTemplate<L, K, V> extends OpenFSMMonit
 				alphabet.makeNewSymbol(new NumberAndSymbol(number,sym), sym.getVariables());
 			}
 		}
+	}
+	
+	public void processEvent(NumberAndSymbol label, IVariableBinding<K,V> binding) {
+		//TODO must have a catch-all mechanism if the number is larger than maxNumber() 
+		super.processEvent(label, binding);
 	}
 	
 	private int maxNumber() {
@@ -192,14 +204,80 @@ public abstract class NumberSyncFSMMonitorTemplate<L, K, V> extends OpenFSMMonit
 	
 	public static void main(String[] args) {
 		FailSafeIterMonitorTemplate fsi = new FailSafeIterMonitorTemplate();
-		NumberSyncFSMMonitorTemplate<String,Var,Object> sync = new NumberSyncFSMMonitorTemplate<String,Var,Object>(fsi) {
+		FailSafeIterMonitorTemplate fsi2 = new FailSafeIterMonitorTemplate();
+		NumberSyncFSMMonitorTemplate<String,Var,Object> sync = new NumberSyncFSMMonitorTemplate<String,Var,Object>(fsi2) {
 			public void matchCompleted(IVariableBinding<Var, Object> binding) {
 				// TODO Auto-generated method stub
 				
 			}
 		};
-		sync.getInitialState();
-		System.out.println(fsi);
-		System.out.println(sync);
+		
+//		System.out.println(fsi);
+//		System.out.println(sync);
+
+		Collection<Object> c = new ArrayList<Object>();
+		Iterator<Object> i = c.iterator();
+		
+		final int MAX = 100000;
+		
+		IVariableBinding<FailSafeIterMonitorTemplate.Var, Object> b_c = new VariableBinding<FailSafeIterMonitorTemplate.Var, Object>();
+		b_c.put(Var.C, c);
+
+		IVariableBinding<FailSafeIterMonitorTemplate.Var, Object> b_i = new VariableBinding<FailSafeIterMonitorTemplate.Var, Object>();
+		b_i.put(Var.I, i);
+
+		IVariableBinding<FailSafeIterMonitorTemplate.Var, Object> b_ci = new VariableBinding<FailSafeIterMonitorTemplate.Var, Object>();
+		b_ci.put(Var.C, c);
+		b_ci.put(Var.I, i);
+		
+		
+//		fsi.processEvent("create", b_ci);
+//		sync.processEvent(sync.new NumberAndSymbol(0, fsi.getSymbolByLabel("create")), b_ci);
+//		fsi.processEvent("update", b_c);
+//		//sync.processEvent(sync.new NumberAndSymbol(0, fsi.getSymbolByLabel("update")), b_c);					
+//		fsi.processEvent("iter", b_i);
+//		sync.processEvent(sync.new NumberAndSymbol(1, fsi.getSymbolByLabel("iter")), b_i);					
+//
+//		System.out.println(fsi.getTrace());
+//		System.out.println(fsi2.getTrace());
+	
+		
+		//System.exit(0);
+		
+		//RANDOMIZED TEST
+
+		Random random = new Random();
+		
+		int missed = 0;
+		for(int n=0; n<MAX; n++) {
+			double rand = Math.random();
+			boolean miss = random.nextBoolean();
+			if(missed>3) miss = false; // TODO how to bound in the general case?
+			if(rand<1/3.0) {
+				fsi.processEvent("create", b_ci);
+				if(miss) {
+					missed++;
+				} else {
+					sync.processEvent(sync.new NumberAndSymbol(missed, fsi2.getSymbolByLabel("create")), b_ci);
+				}
+			} else if(rand>=1/3.0 && rand<2/3.0) {
+				fsi.processEvent("iter", b_i);
+				if(miss) {
+					missed++;
+				} else {
+					sync.processEvent(sync.new NumberAndSymbol(missed, fsi2.getSymbolByLabel("iter")), b_i);					
+				}
+			} else {
+				fsi.processEvent("update", b_c);
+				if(miss) {
+					missed++;
+				} else {
+					sync.processEvent(sync.new NumberAndSymbol(missed, fsi2.getSymbolByLabel("update")), b_c);					
+				}
+			}
+			if(!miss) missed = 0;
+		}
+		System.out.println(fsi.getTrace());
+		System.out.println(fsi2.getTrace());
 	}
 }
